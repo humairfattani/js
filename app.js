9be8996444eab3fee2fa739527ec3b25e3aa0061
humairfattani <humairfattani950@gmail.com>
2022-03-27 09:53:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let abc = {
+    a: 'Humair faisal',
+    b: 18,
+    c: 56,
+}
+console.log(Object.keys(abc))
+let xyz = {
+    l: 'Humair faisal',
+    k: 18,
+    m: 56,
+}
+console.log(Object.values(xyz))
